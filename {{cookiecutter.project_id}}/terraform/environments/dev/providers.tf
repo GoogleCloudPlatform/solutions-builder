04deb62b936681cf6162db6b6af43da27cce0d40
Jon Chen <jonchen@google.com>
2022-09-26 01:17:00 -0400

    update tf provider

--- {{cookiecutter.project_id}}/terraform/environments/dev/providers.tf
+++ {{cookiecutter.project_id}}/terraform/environments/dev/providers.tf
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 # Terraform Block
 terraform {
   required_version = ">= 0.13"
@@ -7,7 +24,7 @@ terraform {
       version = "~>4.0"
     }
     kubectl = {
-      source = "gavinbunney/kubectl"
+      source  = "gavinbunney/kubectl"
       version = ">= 1.14.0"
     }
     helm = {
@@ -33,28 +50,28 @@ data "google_container_cluster" "default" {
 
 # Used by module.gke.
 provider "kubernetes" {
-  host  = "https://${data.google_container_cluster.default.endpoint}"
+  host  = data.google_container_cluster.default.endpoint
   token = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(
-    data.google_container_cluster.default.master_auth[0].cluster_ca_certificate,
+    try(data.google_container_cluster.default.master_auth[0].cluster_ca_certificate, ""),
   )
 }
 
 # Used by module.ingress.
 provider "kubectl" {
-  load_config_file = false
-  host             = "https://${data.google_container_cluster.default.endpoint}"
-  token            = data.google_client_config.default.access_token
+  host  = data.google_container_cluster.default.endpoint
+  token = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(
-    data.google_container_cluster.default.master_auth[0].cluster_ca_certificate,
+    try(data.google_container_cluster.default.master_auth[0].cluster_ca_certificate, ""),
   )
 }
+
 provider "helm" {
   kubernetes {
-    host  = "https://${data.google_container_cluster.default.endpoint}"
+    host  = data.google_container_cluster.default.endpoint
     token = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(
-      data.google_container_cluster.default.master_auth[0].cluster_ca_certificate,
+      try(data.google_container_cluster.default.master_auth[0].cluster_ca_certificate, ""),
     )
   }
 }
